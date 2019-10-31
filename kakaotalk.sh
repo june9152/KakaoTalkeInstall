42288289ba16c7f82d23122bbb736ce46f7c99ad
june9152 <35327235+june9152@users.noreply.github.com>
2019-10-31 14:26:05 +0900

    Create kakaotalk.sh
    sudo apt-get install gnome-tweaks gnome-shell-extensions
    wget -O - https://raw.githubusercontent.com/gyuha/settings/master/applications/kakaotalk.sh | bash

---
+++ kakaotalk.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+wget -O - https://raw.githubusercontent.com/gyuha/settings/master/applications/kakaotalk.sh | bash
+sudo apt install -f fonts-nanum*
+sudo apt install -f wine-stable
+sudo apt install -f gnome-shell-extension-top-icons-plus
+winearch=win32 wineprefix=~/.wine wine wineboot
+cd ~/downloads/
+wget https://raw.githubusercontent.com/winetricks/winetricks/master/src/winetricks
+chmod +x winetricks
+./winetricks riched30 gdiplus msxml6
+sed -i 's/"MS Shell Dlg"="Gulim"/"MS Shell Dlg"="NanumGothic"/' ~/.wine/system.reg
+sed -i 's/"MS Shell Dlg 2"="Tahoma"/"MS Shell Dlg 2"="NanumGothic"/' ~/.wine/system.reg
+wget http://app.pc.kakao.com/talk/win32/KakaoTalk_Setup.exe
+wine ./KakaoTalk_Setup.exe
